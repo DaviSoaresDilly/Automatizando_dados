3a680cb4f50c32c8806a6f79ac528206ed4be4ea
Davi Soares Dilly <93988870+DaviSoaresDilly@users.noreply.github.com>
2024-10-22 20:02:33 -0300

    Refatorar criação e população do banco de dados

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,27 @@
-# main.py
+from sqlalchemy import Engine
 from app.database import get_session
 from app.models import Base
 from app.populate import populate_data
 from app.generate_atendimentos import generate_atendimentos
 
+def reset_database(engine: Engine):
+    """Função para dropar e recriar todas as tabelas"""
+    print("Excluindo todas as tabelas existentes...")
+    Base.metadata.drop_all(engine)  # Excluir todas as tabelas
+
+    print("Criando novas tabelas...")
+    Base.metadata.create_all(engine)  # Criar todas as tabelas
+
 def main():
     session = get_session()
 
-    # Criação das tabelas
-    Base.metadata.create_all(session.bind)
+    # Resetar o banco de dados (drop + create)
+    reset_database(session.bind)
 
     # População dos dados
     populate_data(session)
 
-    # Geração de atendimentos
+    # Geração de atendimentos (simulação com ID)
     generate_atendimentos(session, 52346)
 
 if __name__ == "__main__":
